e1e6ceaff80dd775f9a570359f7081236ab5baf8
Martin Erpicum <martin@erpicum.net>
2023-09-13 04:26:14 +0200

    🚀 init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+   
+    <h3>Informations</h3>
+    <ul>
+      <li><a href="mailto:contact@lincette.be" target="_blank" rel="noopener">contact@lincette.be</a></li>
+      <li><a href="https://www.facebook.com/groups/429847368281109" target="_blank" rel="noopener">facebook</a></li>
+      
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
